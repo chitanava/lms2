335b2f0d088fab0e9606c8e97953ffa0695d6134
Nika Chitanava <chitanava@gmail.com>
2023-04-08 23:01:02 +0400

    add a dropdown menu to the topic action

--- resources/views/components/admin/app.blade.php
+++ resources/views/components/admin/app.blade.php
@@ -35,8 +35,8 @@
           @if (session('status'))
             <div class="alert alert-success shadow-sm">
               <div>
-                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-                <span>{{ session('status') }}</span>
+                <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-5 w-5" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                <span class="text-sm">{{ session('status') }}</span>
               </div>
             </div>
           @endif
